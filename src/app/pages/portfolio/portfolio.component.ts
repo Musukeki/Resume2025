8b144f884186c88fb7a209bab3d6551b3d5abce2
musukeki <musukeki1000@gmail.com>
2025-11-24 23:28:28 +0800

    add portfolio

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { routes } from './../../app.routes';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterOutlet } from '@angular/router';
+
+// ** 引入 masonry 套件 **
+import Masonry from 'masonry-layout';
+
+// ** 資料來源 **
+import jsonData from '../../../assets/portfolio.json';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrls: ['./portfolio.component.scss'],
+  standalone: true,
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements AfterViewInit {
+  @ViewChild('grid') gridRef!: ElementRef;
 
+  portfolioData = jsonData;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    const gridEl = this.gridRef.nativeElement;
+
+    const msnry = new Masonry(gridEl, {
+      itemSelector: '.grid-item',
+      columnWidth: '.grid-sizer',
+      percentPosition: true,
+      gutter: 0,
+    });
+  }
+
+  // 前往作品內頁
+  linkTo(url: any) {
+    // this.router.navigate([link]);
+
+    window.open(url, '_blank');
+  }
 }
